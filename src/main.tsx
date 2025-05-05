0dac49855c5fe6602f3076deaaf3fbff86b8a055
Mimoun <mimounb1597@gmail.com>
2025-05-05 19:02:56 +0200

    feat: Integrate PostHog for web analytics
    Add the PostHog analytics platform to gather usage insights.
    The initial implementation focuses primarily on tracking user counts
    and basic page views.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={options}
+      >
+        <App />
+      </PostHogProvider>
     </ThemeProvider>
   </StrictMode>
 );
